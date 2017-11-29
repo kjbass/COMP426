ebfc8cf3908a5f1aead547412acf07a63f9e2cb9
Kurtis Bass <kurtis.j.bass@gmail.com>
2017-11-28 23:06:28 -0500

    added all create table statements, 'fixed bug' commits coming soon

--- a3/createTables.php
+++ a3/createTables.php
@@ -1,14 +1,53 @@
 <?php
 $conn = new mysqli("classroom.cs.unc.edu", "kjbass", $_GET['pass'], "kjbassdb");
 $conn->query("
-    CREATE TABLE game(
+    CREATE TABLE IF NOT EXISTS teams(
+        teamID INT NOT NULL AUTO_INCREMENT,
+        name VARCHAR(40),
+        date DATE NOT NULL,
+        PRIMARY KEY (teamID)
+    );
+");
+
+$conn->query("
+    CREATE TABLE IF NOT EXISTS games(
         gameID INT NOT NULL AUTO_INCREMENT,
         team1ID INT NOT NULL,
         team2ID INT NOT NULL,
         date DATE NOT NULL,
+        FOREIGN KEY(team1ID) REFERENCES team,
+        FOREIGN KEY(team2ID) REFERENCES team,
         PRIMARY KEY (gameID)
     );
 ");
+
+$conn->query("
+    CREATE TABLE IF NOT EXISTS players(
+        playerID INT NOT NULL AUTO_INCREMENT,
+        firstName VARCHAR(40) NOT NULL,
+        lastName VARCHAR(40) NOT NULL,
+        teamID INT NOT NULL,
+        date DATE NOT NULL,
+        FOREIGN KEY(teamID) REFERENCES team
+        PRIMARY KEY (teamID)
+    );
+");
+$conn->query("
+CREATE TABLE IF NOT EXISTS events(
+    eventID INT NOT NULL AUTO_INCREMENT,
+    event_type VARCHAR(10) NOT NULL,
+    scoringPlayerID INT NOT NULL,
+    quarterBackID INT
+    FOREIGN KEY(teamID) REFERENCES team
+    FOREIGN KEY(scoringPlayerID) REFERENCES players,
+    FOREIGN KEY(quarterBackID) REFERENCES players,
+    PRIMARY KEY (teamID)
+);
+");
+
+
+
+
 $result = $conn->query("SHOW TABLES;");
 echo $result->fetch_row();
 ?>
