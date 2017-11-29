a188ec6f01ec598c31b48da5ecd9cd623648878b
Kurtis Bass <kurtis.j.bass@gmail.com>
2017-11-28 21:25:25 -0500

    adding a test of passing password through get in order to avoid storing my pw in plaintext

--- a3/import.php
+++ a3/import.php
@@ -1,4 +1,7 @@
 <?php
-echo $_GET['test'];
+$conn = new mysqli("classroom.cs.unc.edu", "kjbass", $_GET['pass'], "kjbassdb");
+$conn->query("CREATE TABLE test_table(test VARCHAR(20)");
+$result = $conn->query("SHOW TABLES;");
+echo $result->fetch_row();
 ?>
 
