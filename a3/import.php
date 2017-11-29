a9cfb00f2927c0b93e8c6587019868fcc5fd7d6d
Kurtis Bass <kurtis.j.bass@gmail.com>
2017-11-28 21:26:51 -0500

    classic missing parenthesis in SQL

--- a3/import.php
+++ a3/import.php
@@ -1,6 +1,6 @@
 <?php
 $conn = new mysqli("classroom.cs.unc.edu", "kjbass", $_GET['pass'], "kjbassdb");
-$conn->query("CREATE TABLE test_table(test VARCHAR(20)");
+$conn->query("CREATE TABLE test_table(test VARCHAR(20));");
 $result = $conn->query("SHOW TABLES;");
 echo $result->fetch_row();
 ?>
