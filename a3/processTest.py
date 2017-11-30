12b9445ca3157e5279922175a2836810582c7410
Kurtis Bass <kurtis.j.bass@gmail.com>
2017-11-29 21:35:50 -0500

    adding janky little script for writing the SQL for the players insert statement

---
+++ a3/processTest.py
@@ -0,0 +1,38 @@
+def get_unique_teams(filename):
+    name_set = set()
+    name_to_team = {}
+    data_list = []
+    team_to_teamID = {
+        'Houston' : 1,
+        'Atlanta' : 2,
+        'Dallas' : 3,
+        'Cleveland' : 4,
+        'Seattle' : 5,
+        'NewEngland' : 6,
+        'Oakland' : 7,
+        'GreenBay' : 8,
+        'Carolina' : 9,
+        'Miami' : 10,
+        'Chicago' : 11,
+        'Washington' : 12
+    }
+    with open(filename) as f:
+        for line in f:
+            split = line.split(" ")
+            name = split[0] + ' ' + split[1]
+            if name not in name_set:
+                name_to_team[name] = split[3]
+                print(split[3])
+            name_set.add(name)
+        for name in name_set:
+            split_name = name.split(" ")
+            data_list.append((split_name[0], split_name[1], team_to_teamID[name_to_team[name]]))
+        list_to_SQL(data_list)
+    
+def list_to_SQL(tuple_list):
+    for el in tuple_list:
+        print('''INSERT INTO players(firstName, lastName, teamID) VALUES''' + str(el) + ';')
+
+
+get_unique_teams("input.txt")
+
