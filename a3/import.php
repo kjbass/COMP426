e917af6b810c91cc7c07ee1777692928ea8d63db
Kurtis Bass <kurtis.j.bass@gmail.com>
2017-12-03 16:03:35 -0500

    fleshed out import.php for events

--- a3/import.php
+++ a3/import.php
@@ -1,3 +1,37 @@
 <?php
-    $blah = new Set();
+    $handle = fopen("input.txt", "r");
+    $conn = new mysqli("classroom.cs.unc.edu", "kjbass", "426password!", "kjbassdb");
+    if ($handle) {
+        while (($line = fgets($handle)) !== false) {
+            //FirstName LastName team1 team2 Date EVENTTYPE (FirstName LastName)?
+            if(is_null($last_line)){
+                
+            }
+            $line = $line.explode(" ");
+
+            $result = $conn->query("
+            SELECT g.gameID
+            FROM games g
+            WHERE g.date='".$line[4]."'
+            AND (
+                g.team1ID = (
+                    SELECT t.teamID 
+                    FROM teams t
+                    WHERE t.name='".$line[2]."'
+                )
+                OR
+                g.team1ID = (
+                    SELECT t.teamID 
+                    FROM teams t
+                    WHERE t.name='".$line[3]."'
+                )
+            )
+        ");
+        }
+    
+        fclose($handle);
+    } else {
+        // error opening the file.
+    }
+   
 ?>
